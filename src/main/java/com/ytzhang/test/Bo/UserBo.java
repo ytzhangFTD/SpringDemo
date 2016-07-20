50da7da6fbad51e70f181cae01aac0091889771d
wb-zhangyitong <wb-zhangyitong@alibaba-inc.com>
2016-07-20 10:29:20 +0800

    数据库连接测试通过

--- src/main/java/com/ytzhang/test/Bo/UserBo.java
+++ src/main/java/com/ytzhang/test/Bo/UserBo.java
@@ -1,8 +1,16 @@
 package com.ytzhang.test.Bo;
 
+import com.ytzhang.test.mybatis.model.TestUser;
+
 /**
- * Administrator
- * 2016/7/20
+ * 类UserBo的实现描述：TODO 类实现描述
+ * wb-zhangyitong  2016/7/20 10:23
  */
 public interface UserBo {
+	/**
+	 * 新增用户
+	 *
+	 * @param user
+	 */
+	public void addUser(TestUser user);
 }
--- src/main/java/com/ytzhang/test/Test.java
+++ src/main/java/com/ytzhang/test/Test.java
@@ -1,6 +1,8 @@
 package com.ytzhang.test;
 
+import com.ytzhang.test.Bo.UserBo;
 import com.ytzhang.test.aop.Bussiness;
+import com.ytzhang.test.mybatis.model.TestUser;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
@@ -11,7 +13,11 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 public class Test {
 	public static void main(String[] args) {
 		ApplicationContext applicationContext=new ClassPathXmlApplicationContext("ApplicationContext.xml");
-		Bussiness testBussiness = (Bussiness) applicationContext.getBean("testBussiness");
-		testBussiness.test();
+//		Bussiness testBussiness = (Bussiness) applicationContext.getBean("testBussiness");
+//		testBussiness.test();
+		UserBo userBo = (UserBo) applicationContext.getBean("userBo");
+		TestUser user=new TestUser();
+		user.setName("test");
+		userBo.addUser(user);
 	}
 }
