135d0525b90198d41b399758d3d31a362f9a1480
wb-zhangyitong <wb-zhangyitong@alibaba-inc.com>
2016-08-25 16:15:05 +0800

    抽象工厂模式

---
+++ src/main/java/com/ytzhang/test/builder/abstractfactory/AKBullet.java
@@ -0,0 +1,17 @@
+package com.ytzhang.test.builder.abstractfactory;
+/*
+ * Copyright 2016 Alibaba.com All right reserved. This software is the
+ * confidential and proprietary information of Alibaba.com ("Confidential
+ * Information"). You shall not disclose such Confidential Information and shall
+ * use it only in accordance with the terms of the license agreement you entered
+ * into with Alibaba.com.
+ */
+
+/**
+ * <pre>
+ *  类AKBullet的实现描述：TODO 类实现描述
+ *  wb-zhangyitong  2016/8/25 15:15
+ * </pre>
+ */
+public class AKBullet {
+}
