2cc3ae8e45c2a83571ee1dd7a72beec6908e20f3
wb-zhangyitong <wb-zhangyitong@alibaba-inc.com>
2016-09-27 17:18:32 +0800

    mail test

---
+++ src/main/java/com/ytzhang/test/mail/test/ShowMail.java
@@ -0,0 +1,17 @@
+package com.ytzhang.test.mail.test;
+/*
+ * Copyright 2016 Alibaba.com All right reserved. This software is the
+ * confidential and proprietary information of Alibaba.com ("Confidential
+ * Information"). You shall not disclose such Confidential Information and shall
+ * use it only in accordance with the terms of the license agreement you entered
+ * into with Alibaba.com.
+ */
+
+/**
+ * <pre>
+ *  类ShowMail的实现描述：TODO 类实现描述
+ *  wb-zhangyitong  2016/9/27 17:16
+ * </pre>
+ */
+public class ShowMail {
+}
